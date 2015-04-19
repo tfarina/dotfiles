9f26d74ebc0fc6fe02f83b220cb377ca29c3aa94
Thiago Farina <tfarina@chromium.org>
2015-04-18 22:54:48 -0300

    Use coreutils 'install' command.
    It is more robust than 'cp'.
    
    https://www.gnu.org/software/coreutils/manual/html_node/install-invocation.html
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- bin/install_ninja.sh
+++ bin/install_ninja.sh
@@ -8,7 +8,7 @@ wget https://github.com/martine/ninja/archive/$NINJA_VERSION.tar.gz
 tar -zxvf $NINJA_VERSION.tar.gz
 cd ninja-1.5.3
 ./configure.py --bootstrap
-sudo cp ninja /usr/local/bin/
+sudo install -D --verbose --backup=none --mode=755 -T ninja /usr/local/bin/ninja
 cd ..
 rm -rf ninja-1.5.3
 rm $NINJA_VERSION.tar.gz
