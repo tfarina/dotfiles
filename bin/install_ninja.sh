d659afa12e78777130522fdc6d56f95bd36ac4de
Thiago Farina <tfarina@chromium.org>
2015-04-22 12:25:56 -0300

    Use an alternative URL for downloading ninja tarball.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- bin/install_ninja.sh
+++ bin/install_ninja.sh
@@ -3,12 +3,13 @@
 echo
 echo "Installing Ninja..."
 
-NINJA_VERSION=v1.5.3
-wget https://github.com/martine/ninja/archive/$NINJA_VERSION.tar.gz
+VERSION=1.5.3
+NINJA_VERSION=ninja-$VERSION
+wget http://github.com/martine/ninja/archive/v$VERSION/$NINJA_VERSION.tar.gz
 tar -zxvf $NINJA_VERSION.tar.gz
-cd ninja-1.5.3
+cd $NINJA_VERSION
 ./configure.py --bootstrap
 sudo install -D --verbose --backup=none --mode=755 -T ninja /usr/local/bin/ninja
 cd ..
-rm -rf ninja-1.5.3
+rm -rf $NINJA_VERSION
 rm $NINJA_VERSION.tar.gz
