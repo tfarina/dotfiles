84e9f173624a5f4ee10b47ec4045806ba988f12c
Thiago Farina <tfransosi@gmail.com>
2023-12-03 09:39:05 +0000

    fix installation of elementary icons
    I noticed this while doing another script.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- icons/install_elementary_xfce_icons.sh
+++ icons/install_elementary_xfce_icons.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -r elementary-xfce/ /usr/share/icons/elementary-xfce
-sudo cp -r elementary-xfce-dark/ /usr/share/icons/elementary-xfce-dark
-sudo cp -r elementary-xfce-darker/ /usr/share/icons/elementary-xfce-darker
-sudo cp -r elementary-xfce-darkest/ /usr/share/icons/elementary-xfce/darkest
+sudo cp -r elementary-xfce/ /usr/share/icons
+sudo cp -r elementary-xfce-dark/ /usr/share/icons
+sudo cp -r elementary-xfce-darker/ /usr/share/icons
+sudo cp -r elementary-xfce-darkest/ /usr/share/icons
 
 sudo gtk-update-icon-cache -f /usr/share/icons/elementary-xfce
 sudo gtk-update-icon-cache -f /usr/share/icons/elementary-xfce-dark
