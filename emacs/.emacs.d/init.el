406d4389c97340193a0d28e48b93ce4aed5195fd
Thiago Farina <tfransosi@gmail.com>
2023-12-05 11:50:10 +0000

    emacs: remove blink-cursor-mode setting
    When running the GUI version of Emacs, not on the terminal, we want the cursor
    to blink because that is the way the cursor behaves in Visual Studio 2005.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,9 +18,6 @@
 ;; Hide left scrollbar.
 (scroll-bar-mode -1)
 
-;; Don't blink the cursor.
-(if (fboundp 'blink-cursor-mode) (blink-cursor-mode -1))
-
 ;; Activates the mark and highlights the region.
 (transient-mark-mode t)
 
