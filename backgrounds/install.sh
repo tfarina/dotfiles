88b9286fb24e19c4d52ef4f143643e64d34c3fff
Thiago Farina <tfransosi@gmail.com>
2025-12-30 14:58:39 +0000

    backgrounds: remove legacy install script
    Removed backgrounds/install.sh, as background files are now installed by
    install-assets.sh and wallpaper configuration is handled by
    apply-xfce-theme.sh.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- backgrounds/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo cp leonidas-1-noon.jpg /usr/share/backgrounds
-
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /usr/share/backgrounds/leonidas-1-noon.jpg
